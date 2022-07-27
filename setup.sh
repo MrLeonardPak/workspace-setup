ebe2e49243b20a4736ea4c5851079b4308d71b87
Leonard Pak <38388336+MrLeonardPak@users.noreply.github.com>
2022-07-27 21:11:33 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 # Download Oh My Posh
 wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 chmod +x /usr/local/bin/oh-my-posh
+
+echo -e ". /my-worspace/.x-server\n. /my-workspace/.my-bash" >> $HOME/.bashrc
