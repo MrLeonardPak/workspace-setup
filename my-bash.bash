6cd3645b8b94790d5305ebcc9a1ea58c993c781b
Leonard Pak <leopak2000@gmail.com>
2022-09-10 23:24:13 +0300

    Add git alias

--- my-bash.bash
+++ my-bash.bash
@@ -1,6 +1,10 @@
 # Custom aliases
 alias cdbuild='mkdir build && cd build'
 alias recmake='f(){ cd ../ && rm -rf build/!(_deps) && cd build && cmake "$@" ..; unset -f f; }; f'
+alias ga='git add'
+alias gc='git commit -m'
+alias gs='git status'
+alias gl='git log --pretty=format:"%C(yellow)%h %ad %C(red)| %C(white)%s%C(auto)%d %C(blue)[%an]" --graph --date=short'
 
 # Bash settings
 export HISTCONTROL=ignoredups
