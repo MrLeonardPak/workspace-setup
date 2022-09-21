60ec9664bc09843ca31e1b5c7956381f6fbb5719
Leonard Pak <leopak2000@gmail.com>
2022-09-21 11:42:20 +0300

    Update ros2 bash

--- ros2-bash.bash
+++ ros2-bash.bash
@@ -2,4 +2,4 @@
 . /opt/ros/$ROS_DISTRO/setup.bash
 
 # ROS alias
-alias colconbuild='f(){ colcon build --build-base artifact/build --install-base artifact/install "$@" && . artifact/install/setup.bash; unset -f f; }; f'
+alias colconbuild='f(){ rm -rf artifact && colcon build --build-base artifact/build --install-base artifact/install "$@" && . artifact/install/setup.bash; unset -f f; }; f'
