d4d48143b76b06f91fc75b37931efa47b8197624
Leonard Pak <leopak2000@gmail.com>
2022-09-30 11:17:38 +0300

    Fix

--- setup-bash.bash
+++ setup-bash.bash
@@ -2,4 +2,4 @@
 wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 chmod +x /usr/local/bin/oh-my-posh
 
-echo ". $HOME/workspace-setup/my-bash.bash" >> $HOME/.bashrc
+echo ". $WORKSPACE_USER/workspace-setup/my-bash.bash" >> $WORKSPACE_USER/.bashrc
