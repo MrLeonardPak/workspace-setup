3e235e531188881b561893e003133daa2004c8b6
Leonard Pak <leopak2000@gmail.com>
2022-11-21 09:06:43 +0300

    change ros2 bash

--- ros2-bash.bash
+++ ros2-bash.bash
@@ -1,6 +1,7 @@
-# ROS connect
-. /opt/ros/$ROS_DISTRO/setup.bash
-
 # ROS alias
-alias colconbuild='f(){ colcon build --build-base artifact/build --install-base artifact/install "$@" && . artifact/install/setup.bash; unset -f f; }; f'
-alias ros2pkgsetup='apt update && rosdep install -i --from-path src --rosdistro $ROS_DISTRO -y'
+alias ros-setup='f(){ . /opt/ros/"$@"/setup.bash; unset -f f; }; f'
+alias ros2-build='f(){ colcon build "$@" && . install/setup.bash; unset -f f; }; f'
+alias ros2-build-clear='f(){ rm -rf install/ build/ log/ && colcon build "$@" && . install/setup.bash; unset -f f; }; f'
+alias ros2-build-debug='f(){ colcon build --build-base artifact/build --install-base artifact/install --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Debug "$@" && . artifact/install/setup.bash; unset -f f; }; f'
+alias ros2-debug='f(){ ros2 run --prefix "gdbserver localhost:3000" "$@"; unset -f f; }; f'
+alias ros2-install-pkg='f(){ sudo apt update && rosdep install -i --from-path src -y --rosdistro "$@"; unset -f f; }; f'
