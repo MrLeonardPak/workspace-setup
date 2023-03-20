3617fae814576fddbc166f6d06b4fb8e5324e0c6
Leonard Pak <leopak2000@gmail.com>
2023-03-20 20:27:47 +0300

    update ros2 bash

--- ros2-bash.bash
+++ ros2-bash.bash
@@ -1,7 +1,7 @@
 # ROS alias
 alias ros-setup='f(){ . /opt/ros/"$@"/setup.bash; unset -f f; }; f'
-alias ros2-build='f(){ colcon build "$@" && . install/setup.bash; unset -f f; }; f'
-alias ros2-build-clear='f(){ rm -rf install/ build/ log/ && colcon build "$@" && . install/setup.bash; unset -f f; }; f'
-alias ros2-build-debug='f(){ colcon build --build-base artifact/build --install-base artifact/install --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Debug "$@" && . artifact/install/setup.bash; unset -f f; }; f'
+alias ros2-build='f(){ colcon build --symlink-install "$@" && . install/setup.bash; unset -f f; }; f'
+alias ros2-build-clear='f(){ rm -rf install/ build/ && colcon build "$@" && . install/setup.bash; unset -f f; }; f'
+alias ros2-build-debug='f(){ colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Debug "$@" && . install/setup.bash; unset -f f; }; f'
 alias ros2-debug='f(){ ros2 run --prefix "gdbserver localhost:3000" "$@"; unset -f f; }; f'
 alias ros2-install-pkg='f(){ sudo apt update && rosdep install -i --from-path src -y --rosdistro "$@"; unset -f f; }; f'
