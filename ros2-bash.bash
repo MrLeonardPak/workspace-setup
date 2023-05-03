af66236c333a0c36631ec3ef84fc04d602058b85
Leonard Pak <leopak2000@gmail.com>
2023-05-03 13:41:37 +0300

    update ros bash\

--- ros2-bash.bash
+++ ros2-bash.bash
@@ -1,7 +1,9 @@
 # ROS alias
 alias ros-setup='f(){ . /opt/ros/"$@"/setup.bash; unset -f f; }; f'
-alias ros2-build='f(){ colcon build --symlink-install "$@" && . install/setup.bash; unset -f f; }; f'
+alias ros2-build='f(){ colcon build "$@" && . install/setup.bash; unset -f f; }; f'
 alias ros2-build-clear='f(){ rm -rf install/ build/ && colcon build "$@" && . install/setup.bash; unset -f f; }; f'
-alias ros2-build-debug='f(){ colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Debug "$@" && . install/setup.bash; unset -f f; }; f'
+alias ros2-build-debug='f(){ colcon build --cmake-args -DCMAKE_BUILD_TYPE=Debug "$@" && . install/setup.bash; unset -f f; }; f'
 alias ros2-debug='f(){ ros2 run --prefix "gdbserver localhost:3000" "$@"; unset -f f; }; f'
 alias ros2-install-pkg='f(){ sudo apt update && rosdep install -i --from-path src -y --rosdistro "$@"; unset -f f; }; f'
+# ROS macros
+export RCUTILS_COLORIZED_OUTPUT=1
