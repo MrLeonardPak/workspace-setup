237a9e0d93782ccc1826a89aa8405bb6f4398003
Leonard Pak <leopak2000@gmail.com>
2023-09-04 12:00:19 +0300

    change ps alias

--- powershell/user-profile.ps1
+++ powershell/user-profile.ps1
@@ -11,13 +11,7 @@ Set-Alias vim nvim
 Set-Alias ll ls
 Set-Alias grep findstr
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
-Function F1 { git add }
-# Set-Alias ga F1
-Function F2 { git commit -m }
-# Set-Alias gcm F2
 Function F3 { git status -s }
-# Set-Alias gs F3
+Set-Alias gs F3
 Function F4 { git log --pretty=format:"%C(yellow)%h %ad %C(red)| %C(white)%s%C(auto)%d %C(blue)[%an]" --graph --date=short }
 Set-Alias -Name glg -Value F4
-Function F5 { git diff }
-Set-Alias gd F5
